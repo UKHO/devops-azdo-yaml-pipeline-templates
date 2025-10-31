89fe63392c141b1516e5b15d60242917af26372e
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2025-10-31 15:10:37 +0000

    Update variable scoping experiment with dynamic variable groups and branch reference fix

--- tests/experiments/terraform/main.tf
+++ tests/experiments/terraform/main.tf
@@ -4,12 +4,23 @@ data "azuredevops_project" "this" {
 
 data "azuredevops_serviceendpoint_github" "this" {
   project_id          = data.azuredevops_project.this.project_id
-  service_endpoint_id = "f24dd0d2-8d08-4fa6-b54c-4d57e7add104" # Got this via the 'UKHO' service connection's page
+  service_endpoint_id = "d01ddbe4-5df4-4284-8dce-448c0a40eb5e" # Got this via the 'UKHO' service connection's page
+}
+
+locals {
+  variableGroups = {
+    forPipeline = "ForPipeline",
+    forStage = "ForStage",
+    forJob = "ForJob"
+  }
 }
 
 resource "azuredevops_variable_group" "this" {
   project_id   = data.azuredevops_project.this.project_id
-  name         = "Experimental-VariablesAndScope"
+
+  for_each = local.variableGroups
+
+  name         = "Experimental-VariablesAndScope-${each.value}"
   description  = "An experimental variable group that is safe for deletion because it is being used in an experimental branch: experiment/how-do-variables-act-at-different-scopes"
   allow_access = true
 
@@ -33,7 +44,7 @@ resource "azuredevops_build_definition" "this" {
   repository {
     repo_type             = "GitHub"
     repo_id               = "UKHO/devops-azdo-yaml-pipeline-templates"
-    branch_name           = "experiment/how-do-variables-act-at-different-scopes"
+    branch_name           = "refs/heads/experiment/how-do-variables-act-at-different-scopes"
     yml_path              = "tests/experiments/how-do-variables-act-at-different-scopes.yml"
     service_connection_id = data.azuredevops_serviceendpoint_github.this.service_endpoint_id
   }
