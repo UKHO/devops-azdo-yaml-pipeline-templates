fdac27c63a461412a9bb8d45f4710ffe4c2e3565
Scott James <scott.james@ukho.gov.uk>
2025-12-05 09:37:55 +0000

    Removed experimental Terraform pipeline configuration and related resources.

--- tests/pipelines/experimental_pipelines/detect_changes/pipeline_terraform/providers.tf
+++
@@ -1,4 +0,0 @@
-# https://registry.terraform.io/providers/microsoft/azuredevops/latest/docs#argument-reference
-provider "azuredevops" {
-  org_service_url = "https://dev.azure.com/ukhydro" # Required for finding the azure devops server, combined with entra id login
-}
