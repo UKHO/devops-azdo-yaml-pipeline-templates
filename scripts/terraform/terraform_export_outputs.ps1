1974abe4ffd34bdf30e591290ec1c73d1ecf5267
Scott James <scott.james@ukho.gov.uk>
2025-12-08 09:34:14 +0000

    Exported Terraform outputs to JSON file and added script to handle output variable extraction in apply job.

---
+++ scripts/terraform/terraform_export_outputs.ps1
@@ -0,0 +1,40 @@
+param (
+  [Parameter(Mandatory,
+    HelpMessage = "Specify the Terraform output variables to export, separated by spaces.")]
+  [ValidateNotNullOrEmpty()]
+  [string] $OutputVariablesToExport,
+
+  [Parameter(Mandatory,
+    HelpMessage = "Path to the Terraform output file for extracting the output variables.")]
+  [ValidateScript(
+    { -not (Test-Path -Path $OutputFilePath) },
+    ErrorMessage = '"{0}" cannot be found.'
+  )]
+  [string] $OutputFilePath
+)
+
+Write-Host "Starting terraform_export_outputs.ps1 script"
+
+$terraformOutputVariables = Get-Content -Path $OutputFilePath | ConvertFrom-Json
+Write-Output "Exporting required variables for deployment"
+
+foreach ($outputVariableToExport in $OutputVariablesToExport -split " ")
+{
+  Write-Host "Exporting '$outputVariableToExport' variable from terraform output."
+
+  if ( $terraformOutputVariables.ContainsKey($outputVariableToExport))
+  {
+    $output = $terraformOutputVariables.$outputVariableToExport.value
+    Write-Host "Found variable '$outputVariableToExport' with value: $output"
+    Write-Host "##vso[task.setvariable variable=$outputVariableToExport;isoutput=true]$output"
+    Write-Host "Exported."
+  }
+  else
+  {
+    Write-Host "##[error]Cannot find variable '$outputVariableToExport' inside of '$OutputFilePath'"
+    Write-Host "Script failed: missing variable."
+    return
+  }
+}
+
+Write-Host "Script completed: exported outputs."
