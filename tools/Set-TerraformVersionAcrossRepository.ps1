0b9206bc1ed411e4629c37689616cf402632977a
Scott James <scott.james@ukho.gov.uk>
2026-01-07 08:20:40 +0000

    Set terraform version to 1.14.0 instead of latest

---
+++ tools/Set-TerraformVersionAcrossRepository.ps1
@@ -0,0 +1,28 @@
+$path = "C:\Git\devops-azdo-yaml-pipeline-templates"
+$newValue = "1.14.0"
+
+Get-ChildItem $path -Recurse -Filter *.yml | ForEach-Object {
+  $content = Get-Content $_.FullName
+
+  $updated = $false
+
+  for ($i = 0; $i -lt $content.Count; $i++) {
+    if ($content[$i] -match '^\s*-?\s*name:\s*TerraformVersion\s*$') {
+      for ($j = $i + 1; $j -lt $content.Count; $j++) {
+        if ($content[$j] -match '^\s*default:\s*') {
+          $indent = ($content[$j] -replace '(default:.*)$', '')
+          $content[$j] = "${indent}default: '$newValue'"
+          $updated = $true
+          break
+        }
+        if ($content[$j] -match '^\s*-?\s*name:\s*') {
+          break
+        }
+      }
+    }
+  }
+
+  if ($updated) {
+    Set-Content $_.FullName $content
+  }
+}
