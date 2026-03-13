31766a0139b647dea30f9fb881710e01bc628d3a
Scott James <scott.james@ukho.gov.uk>
2026-03-13 16:11:50 +0000

    Simplify Terraform test configuration: remove `random` provider, adjust pipeline name/path, and update module dependencies.

--- tests/terraform_azdo/main.tf
+++ tests/terraform_azdo/main.tf
@@ -7,20 +7,15 @@ data "azuredevops_serviceendpoint_github" "this" {
   service_endpoint_name = var.github_serviceconnection_name
 }
 
-resource "random_string" "this" {
-  length = 8
-  lower  = true
-}
-
 resource "azuredevops_build_definition" "this" {
   project_id = data.azuredevops_project.this.project_id
-  name       = "unit_test_${random_string.this.result}"
-  path       = "\\${local.repository_name}\\unit_tests"
+  name       = "compile_pipeline"
+  path       = "\\${local.repository_name}"
 
   repository {
     repo_type             = "GitHub"
     repo_id               = local.repository_id
-    yml_path              = "tests/unit_tests/terraform_azdo/empty_pipeline.yml"
+    yml_path              = "tests/terraform_azdo/empty_pipeline.yml"
     branch_name           = "refs/heads/${var.target_branch}"
     service_connection_id = data.azuredevops_serviceendpoint_github.this.id
   }
