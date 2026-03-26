12433b4fac1a4f781c8310f267e61d8b8c2b57cc
Scott James <scott.james@ukho.gov.uk>
2026-03-26 08:16:42 +0000

    Simplify Terraform test cases by removing redundant parameters, consolidating backend configurations, and streamlining command descriptions.

--- tasks/terraform.Tests.ps1
+++ tasks/terraform.Tests.ps1
@@ -10,22 +10,21 @@ if (-not (Get-Command -Name 'Run-Tests' -ErrorAction SilentlyContinue))
 # ============================================================================
 
 $validTestCases = @(
-  # INIT Command Tests
+# INIT Command Tests
   @{
-    Description = "init command without service connection (uses script)"
+    Description = "init command"
     Parameters = @{
       Command = "init"
     }
     ExpectedYaml = @(
       'terraform init'
-      'displayName: Terraform init'
     )
   },
   @{
-    Description = "init command without service connection and without backend (DisableBackend=true)"
+    Description = "init command without backend"
     Parameters = @{
       Command = "init"
-      DisableBackend = "true"
+      CommandOptions = "-backend=false"
     }
     ExpectedYaml = @(
       'terraform init'
@@ -36,35 +35,14 @@ $validTestCases = @(
     Description = "init command with full backend configuration"
     Parameters = @{
       Command = "init"
-      BackendAzureServiceConnection = "my-service-connection"
-      BackendAzureResourceGroupName = "my-rg"
-      BackendAzureStorageAccountName = "mystgacct"
-      BackendAzureContainerName = "tfstate"
-      BackendAzureBlobName = "terraform.tfstate"
-      EnvironmentAzureServiceConnection = "azure-subscription-connection"
+      CommandOptions = '-backend-config="key=terraform.tfstate" -backend-config="container_name=tfstate" -backend-config="storage_account_name=mysa" -backend-config="resource_group_name=my-rg"'
     }
     ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: init"
-      "provider: 'azurerm'"
-      "backendServiceArm: my-service-connection"
-      "backendAzureRmResourceGroupName: my-rg"
-      "backendAzureRmStorageAccountName: mystgacct"
-      "backendAzureRmContainerName: tfstate"
-      "backendAzureRmKey: terraform.tfstate"
-    )
-  },
-  @{
-    Description = "init command with backend service connection only"
-    Parameters = @{
-      Command = "init"
-      EnvironmentAzureServiceConnection = "my-service-connection"
-      BackendAzureServiceConnection = "my-service-connection"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: init"
-      "backendServiceArm: my-service-connection"
+      "terraform init"
+      '-backend-config="resource_group_name=my-rg"'
+      '-backend-config="storage_account_name=mysa"'
+      '-backend-config="container_name=tfstate"'
+      '-backend-config="key=terraform.tfstate"'
     )
   },
 
@@ -73,181 +51,175 @@ $validTestCases = @(
     Description = "validate command"
     Parameters = @{
       Command = "validate"
-      WorkingDirectory = "/path/to/terraform"
     }
     ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: validate"
-      "provider: 'azurerm'"
-      "workingDirectory: /path/to/terraform"
+      "terraform validate"
+    )
+  },
+
+  @{
+    Description = "validate command with command options"
+    Parameters = @{
+      Command = "validate"
+      CommandOptions = "-no-color"
+    }
+    ExpectedYaml = @(
+      "terraform validate"
+      "-no-color"
     )
   },
 
   # PLAN Command Tests
   @{
-    Description = "plan command with environment service connection and command options"
+    Description = "plan command and command options"
     Parameters = @{
       Command = "plan"
-      EnvironmentAzureServiceConnection = "azure-subscription-connection"
-      CommandOptions = "-out=tfplan -input=false"
+      CommandOptions = "-out=tfplan `$(VariableFilesCommandOption) -input=false"
     }
     ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: plan"
-      "environmentServiceNameAzureRM: azure-subscription-connection"
-      "commandOptions: -out=tfplan -input=false"
+      "terraform plan"
+      "-out=tfplan `$(VariableFilesCommandOption) -input=false"
     )
   },
-<#  @{ # Cant get the TaskEnvironmentVariables to render in the compiled YAML, needs investigation
-    Description = "plan command with task environment variables"
+  # APPLY Command Tests
+  @{
+    Description = "apply command"
     Parameters = @{
-      Command = "plan"
-      EnvironmentAzureServiceConnection = "azure-subscription-connection"
+      Command = "apply"
+      CommandOptions = '$(VariableFilesCommandOption) -input=false'
+    }
+    ExpectedYaml = @(
+      "terraform apply"
+      '$(VariableFilesCommandOption) -input=false'
+    )
+  },
+
+    # OUTPUT Command Tests
+    @{
+      Description = "output command to console"
+      Parameters = @{
+        Command = "output"
+        OutputTo = "console"
+      }
+      ExpectedYaml = @(
+        "terraform output"
+        "outputTo: console"
+      )
+    },
+    @{
+      Description = "output command to file with filename"
+      Parameters = @{
+        Command = "output"
+        OutputTo = "file"
+        OutputFileName = "terraform-output.json"
+      }
+      ExpectedYaml = @(
+        "terraform output"
+        "outputTo: file"
+        "fileName: terraform-output.json"
+      )
+    },
+  #
+  #  # SHOW Command Tests
+  #  @{
+  #    Description = "show command with default format"
+  #    Parameters = @{
+  #      Command = "show"
+  #    }
+  #    ExpectedYaml = @(
+  #      "command: show"
+  #      "outputTo: console"
+  #    )
+  #  },
+  #  @{
+  #    Description = "show command with json output format"
+  #    Parameters = @{
+  #      Command = "show"
+  #      OutputFormat = "json"
+  #      OutputTo = "file"
+  #      OutputFileName = "plan.json"
+  #    }
+  #    ExpectedYaml = @(
+  #      "command: show"
+  #      "outputTo: file"
+  #      "fileName: plan.json"
+  #      "outputFormat: json"
+  #    )
+  #  },
+  #
+  #  # DESTROY Command Test
+  #  @{
+  #    Description = "destroy command with environment service connection"
+  #    Parameters = @{
+  #      Command = "destroy"
+  #      CommandOptions = "-auto-approve"
+  #    }
+  #    ExpectedYaml = @(
+  #      "command: destroy"
+  #      "commandOptions: -auto-approve"
+  #    )
+  #  },
+  #
+    # CUSTOM Command Test
+    @{
+      Description = "custom command"
+      Parameters = @{
+        Command = "custom"
+        CustomCommand = "version"
+      }
+      ExpectedYaml = @(
+        "terraform version"
+      )
+    },
+  #
+  # Task Naming and Retry Tests
+  @{
+    Description = "command with task environment variables"
+    Parameters = @{
+      Command = "init"
       TaskEnvironmentVariables = @{
         TF_VAR_environment = "dev"
         TF_LOG = "DEBUG"
       }
     }
     ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: plan"
-      "environmentServiceNameAzureRM: azure-subscription-connection"
-    )
-  },#>
-
-  # APPLY Command Tests
-  @{
-    Description = "apply command with environment service connection"
-    Parameters = @{
-      Command = "apply"
-      EnvironmentAzureServiceConnection = "azure-subscription-connection"
-      CommandOptions = "-input=false"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: apply"
-      "environmentServiceNameAzureRM: azure-subscription-connection"
-      "commandOptions: -input=false"
+      "TF_VAR_environment: dev"
+      "TF_LOG: DEBUG"
     )
   },
   @{
-    Description = "apply command with working directory"
+    Description = "command with correct displayName"
     Parameters = @{
-      Command = "apply"
-      WorkingDirectory = "`$(TerraformWorkingDirectory)"
+      Command = "init"
     }
     ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: apply"
-      "workingDirectory: `$(TerraformWorkingDirectory)"
-    )
-  },
-
-  # OUTPUT Command Tests
-  @{
-    Description = "output command to console"
-    Parameters = @{
-      Command = "output"
-      OutputTo = "console"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: output"
-      "outputTo: console"
+      'displayName: Terraform init'
     )
   },
   @{
-    Description = "output command to file with filename"
+    Description = "command with condition"
     Parameters = @{
-      Command = "output"
-      EnvironmentAzureServiceConnection = "azure-connection"
-      OutputTo = "file"
-      OutputFileName = "terraform-output.json"
+      Command = "init"
+      Condition = "failure()"
     }
     ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: output"
-      "outputTo: file"
-      "fileName: terraform-output.json"
-      "environmentServiceNameAzureRM: azure-connection"
-    )
-  },
-
-  # SHOW Command Tests
-  @{
-    Description = "show command with default format"
-    Parameters = @{
-      Command = "show"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: show"
-      "outputTo: console"
+      'condition: failure()'
     )
   },
   @{
-    Description = "show command with json output format"
-    Parameters = @{
-      Command = "show"
-      OutputFormat = "json"
-      OutputTo = "file"
-      OutputFileName = "plan.json"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: show"
-      "outputTo: file"
-      "fileName: plan.json"
-      "outputFormat: json"
-    )
-  },
-
-  # DESTROY Command Test
-  @{
-    Description = "destroy command with environment service connection"
-    Parameters = @{
-      Command = "destroy"
-      EnvironmentAzureServiceConnection = "azure-subscription-connection"
-      CommandOptions = "-auto-approve"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: destroy"
-      "environmentServiceNameAzureRM: azure-subscription-connection"
-      "commandOptions: -auto-approve"
-    )
-  },
-
-  # CUSTOM Command Test
-  @{
-    Description = "custom command"
-    Parameters = @{
-      Command = "custom"
-      CustomCommand = "version"
-    }
-    ExpectedYaml = @(
-      '- task: TerraformTask@5'
-      "command: custom"
-      "customCommand: version"
-    )
-  },
-
-  # Task Naming and Retry Tests
-  @{
-    Description = "init with unique task slug"
+    Description = "command with unique task slug"
     Parameters = @{
       Command = "init"
       TaskNameUniqueSlug = "primary"
-      DisableBackend = "true"
     }
     ExpectedYaml = @(
       'name: TerraformTask_init_primary'
     )
   },
   @{
-    Description = "plan command with retry count"
+    Description = "command with retry count"
     Parameters = @{
-      Command = "plan"
+      Command = "init"
       RetryCountOnTaskFailure = 2
     }
     ExpectedYaml = @(
