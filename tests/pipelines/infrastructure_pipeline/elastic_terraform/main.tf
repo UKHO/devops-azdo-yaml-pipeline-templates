cd4fe9c5bce8b0c92b41e7cbb0e0813adffa83e8
Scott James <scott.james@ukho.gov.uk>
2026-04-01 15:24:48 +0100

    Update Elastic Stack Terraform configuration: Adjust SLO settings, refine endpoints, and add tags for testing

--- tests/pipelines/infrastructure_pipeline/elastic_terraform/main.tf
+++ tests/pipelines/infrastructure_pipeline/elastic_terraform/main.tf
@@ -10,19 +10,27 @@ terraform {
 }
 
 provider "elasticstack" {
-  elasticsearch {}
-  kibana {}
+  elasticsearch {
+    endpoints = [
+      "https://ukho-nonlive-elastic.es.uksouth.azure.elastic-cloud.com"
+    ]
+  }
+  kibana {
+    endpoints = [
+      "https://ukho-nonlive-elastic.kb.uksouth.azure.elastic-cloud.com:9243"
+    ]
+  }
 }
 
 resource "elasticstack_kibana_slo" "custom_kql" {
-  name        = "custom kql devops chapter"
-  description = "custom kql devops chapter"
+  name        = "test-slo-devopschapter"
+  description = "SLO terraform test"
 
   kql_custom_indicator {
-    index  = "metrics-apm*,synthetics*"
-    good   = "summary.up:1"
-    total  = "summary.up:*"
-    filter = "monitor.name:\"File Share Service - Website - Testing - DownloadFile\""
+    index  = "logs-*"
+    good   = "http.response.status_code >= 200 and http.response.status_code < 300"
+    total  = "http.response.status_code >= 100"
+    filter = ""
   }
 
   time_window {
@@ -33,8 +41,8 @@ resource "elasticstack_kibana_slo" "custom_kql" {
   budgeting_method = "occurrences"
 
   objective {
-    target           = 0.95
-    timeslice_target = 0.95
+    target           = 0.99
+    timeslice_target = 0.99
     timeslice_window = "5m"
   }
 
@@ -42,4 +50,6 @@ resource "elasticstack_kibana_slo" "custom_kql" {
     sync_delay = "5m"
     frequency  = "5m"
   }
+
+  tags = ["testing", "availability", "terraform"]
 }
