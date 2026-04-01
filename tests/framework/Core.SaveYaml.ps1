c2aedeb97c38d5ed1cfa46d9ebae484b17bcfc16
Scott James <scott.james@ukho.gov.uk>
2026-04-01 11:22:02 +0100

    Add support for saving compiled YAML to disk for debugging and inspection

--- tests/framework/Core.SaveYaml.ps1
+++ tests/framework/Core.SaveYaml.ps1
@@ -8,21 +8,27 @@ function Save-CompiledYamlToFile
   <#
   .SYNOPSIS
     Saves compiled YAML output to disk in an organized directory structure.
+
   .DESCRIPTION
     When enabled, saves the compiled YAML from Azure DevOps API to disk in
     a template-specific folder adjacent to the test file. This is useful for
     debugging complex templates, version control tracking, or regression testing.
+
   .PARAMETER TestFilePath
     The full path to the test file (PS1) that is running. Used to determine
     the output directory location.
+
   .PARAMETER TemplateName
     The name of the template being tested. Used as the output folder name.
     Example: "windows_test", "terraform_build"
+
   .PARAMETER TestCaseDescription
     The description of the test case. Used as the filename (sanitized).
     Example: "with default parameters"
+
   .PARAMETER CompiledYaml
     The compiled YAML content returned from Azure DevOps API.
+
   .EXAMPLE
     Save-CompiledYamlToFile -TestFilePath "C:\repo\tests\windows_test.ps1" `
       -TemplateName "windows_test" `
