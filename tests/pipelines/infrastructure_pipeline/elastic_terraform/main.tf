402aea25c1e0649036f85e75657b070884f64458
Scott James <scott.james@ukho.gov.uk>
2026-04-14 11:35:55 +0100

    Update `name` field in `elasticstack_kibana_slo` resource for testing purposes in `main.tf`.

--- tests/pipelines/infrastructure_pipeline/elastic_terraform/main.tf
+++ tests/pipelines/infrastructure_pipeline/elastic_terraform/main.tf
@@ -21,7 +21,7 @@ provider "elasticstack" {
 }
 
 resource "elasticstack_kibana_slo" "custom_kql" {
-  name        = "test-slo-devopschapter"
+  name        = "test-slo-devopschapter-2"
   description = "SLO terraform test"
 
   kql_custom_indicator {
