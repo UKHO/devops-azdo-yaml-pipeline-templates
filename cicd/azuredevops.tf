f1f40a6b8427b52271ebb722ba885cda3ccd08dc
Scott James <scott.james@ukho.gov.uk>
2026-04-16 10:54:22 +0100

    Refactor: Simplify `azuredevops.tf` by removing unused `moved` block and streamlining `build_definition` resource

--- cicd/azuredevops.tf
+++ cicd/azuredevops.tf
@@ -1,4 +1,3 @@
-
 data "azuredevops_project" "this" {
   name = var.AzDO_Project_Name
 }
@@ -6,11 +5,6 @@ data "azuredevops_project" "this" {
 data "azuredevops_serviceendpoint_github" "this" {
   project_id          = data.azuredevops_project.this.project_id
   service_endpoint_id = var.GitHub_ServiceConnection_Guid
-}
-
-moved {
-  from = azuredevops_build_definition.this
-  to   = azuredevops_build_definition.pipeline_template_tests
 }
 
 resource "azuredevops_build_definition" "pipeline_template_tests" {
