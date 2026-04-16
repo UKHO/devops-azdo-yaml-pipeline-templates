43ee6e876e946bf0306c652eb6560c69225f4b72
Scott James <scott.james@ukho.gov.uk>
2026-04-16 09:50:19 +0100

    Refactor: Consolidate Terraform configurations into `main.tf` for improved structure and consistency in CI/CD setup

--- cicd/azuredevops.tf
+++ cicd/azuredevops.tf
@@ -1,3 +1,13 @@
+
+data "azuredevops_project" "this" {
+  name = var.AzDO_Project_Name
+}
+
+data "azuredevops_serviceendpoint_github" "this" {
+  project_id          = data.azuredevops_project.this.project_id
+  service_endpoint_id = var.GitHub_ServiceConnection_Guid
+}
+
 moved {
   from = azuredevops_build_definition.this
   to   = azuredevops_build_definition.pipeline_template_tests
--- cicd/main.tf
+++ cicd/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">= 1.11.0"
+    }
+  }
+
+  backend "local" {
+    path = "state/terraform.tfstate"
+  }
+}
+
 # https://registry.terraform.io/providers/microsoft/azuredevops/latest/docs#argument-reference
 provider "azuredevops" {
   org_service_url = "https://dev.azure.com/ukhydro" # Required for finding the azure devops server, combined with entra id login
