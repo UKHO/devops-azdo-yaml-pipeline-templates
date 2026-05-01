357da3a411e3c3de0122e3881c6f49d8f174c337
Scott James <scott.james@ukho.gov.uk>
2026-05-01 13:42:58 +0100

    Remove Kibana SLO resource from Elastic test Terraform configuration.

--- tests/shared_resources/elastic_test_terraform/main.tf
+++ tests/shared_resources/elastic_test_terraform/main.tf
@@ -51,34 +51,3 @@ resource "elasticstack_kibana_slo" "custom_kql" {
 
   tags = ["testing", "availability", "terraform"]
 }
-resource "elasticstack_kibana_slo" "custosdadm_kql" {
-  name        = "test-slo-devopschapter-2adsasd"
-  description = "SLO terraform test"
-
-  kql_custom_indicator {
-    index  = "logs-*"
-    good   = "http.response.status_code >= 200 and http.response.status_code < 300"
-    total  = "http.response.status_code >= 100"
-    filter = ""
-  }
-
-  time_window {
-    duration = "30d"
-    type     = "rolling"
-  }
-
-  budgeting_method = "occurrences"
-
-  objective {
-    target           = 0.99
-    timeslice_target = 0.99
-    timeslice_window = "5m"
-  }
-
-  settings {
-    sync_delay = "5m"
-    frequency  = "5m"
-  }
-
-  tags = ["testing", "availability", "terraform"]
-}
